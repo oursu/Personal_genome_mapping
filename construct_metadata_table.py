b43016c4fecd05c795962c551cd97240dacab069
Oana Maria Ursu <oursu@scg3-ln01.local>
2014-07-15 18:10:34 -0700

    Added GM19193 gender hard-coded

--- construct_metadata_table.py
+++ construct_metadata_table.py
@@ -9,13 +9,13 @@ Author:Oana Ursu
 
 def main():
     parser=OptionParser()
-    parser.add_option('--GM_vs_NA',dest='gm_or_na')
+    parser.add_option('--GM_vs_NA',dest='gm_or_na',default='NA',help='Whether to use GM or NA. DEfault is NA')
     parser.add_option('--gender_file',dest='gender_file')
     parser.add_option('--out',dest='out',help='Out')
     parser.add_option('--genome_path',dest='genome_path',help='Genome path')
     parser.add_option('--vcf_file',dest='vcf_file',help='VCF file with all individuals')
     parser.add_option('--align_dir',dest='alignment_directory',help='Alignment directory')
-    parser.add_option('--people_data',dest='people_data',help='Sample to fasta files. Col1 = sample (should include individual name either NA19099 or GM19099). Col2: fasta full path')
+    parser.add_option('--people_data',dest='people_data',help='Sample to fasta files. Col1 = sample (should include individual name either NA19099 or GM19099). Col2: fasta full path. If sample name is repeated, all fasta files will be assigned to that sample name. NOTE: Only 2 fasta files per sample name are supported currently')
     opts,args=parser.parse_args()
 
     di={}
@@ -41,31 +41,28 @@ def main():
         gender=items[4]
         famid=items[1]
         popul=items[2]
+        person=re.sub('NA',opts.gm_or_na,person)
         if person not in gender_di.keys():
             gender_di[person]={}
         if person in gender_di.keys():
             gender_di[person]['gender']=gender
             gender_di[person]['famid']=famid
             gender_di[person]['population']=popul
-
+    gender_di['GM19193']={}
+    gender_di['GM19193']['gender']='female'
     #Write the dictionary to a file
     out=open(opts.out,'w')
     out.write('#Individual\tSampleName\tFQ1\tFQ2\tGenomePath\tGender\tVcfFile\tAlignmentDirectory\n')
     for id_name in di.keys():
         genome_path=opts.genome_path
         alignment_directory=opts.alignment_directory
-        print id_name
         g='NA'
-        if id_name in gender_di.keys():                                                                                                                         
-            if 'gender' in gender_di[id_name].keys():  
-                g=gender_di[id_name]['gender']
-        print g
+        if di[id_name]['person'] in gender_di.keys():                                                                                                                         
+            if 'gender' in gender_di[di[id_name]['person']].keys():  
+                g=gender_di[di[id_name]['person']]['gender']
         fastqs=['NA','NA']
-        print di[id_name]['fastq']
         for i in range(len(di[id_name]['fastq'])):
             fastqs[i]=di[id_name]['fastq'][i]
-        print fastqs
-        print di[id_name]
         out.write(di[id_name]['person']+'\t'+id_name+'\t'+fastqs[0]+'\t'+fastqs[1]+'\t'+opts.genome_path+'\t'+g+'\t'+opts.vcf_file+'\t'+opts.alignment_directory+'\n')
-
+    print "Done========="
 main()
